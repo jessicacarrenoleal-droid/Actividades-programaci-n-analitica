a44343a5f7d9109db86dc75328cb525271cc5bf4
jessicacarrenoleal-droid <jessica.carreno.leal@gmail.com>
2026-09-04 21:42:28 -0500

    Add files via upload

---
+++ ejercico_analitica.py
@@ -0,0 +1,11 @@
+# %% ejemplo listas
+
+compress=["pan","leche"]
+compras.append("huevos")
+print(compras)
+# ["pan", "leche", "huevos"]
+compras.insert(0,"cafe")
+compras.remove("leche")
+ultimo=compras.pop()
+print(ultimo)# huevos
+print(compras)# ["cafe", "pan"]
